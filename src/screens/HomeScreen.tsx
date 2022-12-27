88c02c08b66ad272f554d6d3ee1f32b216f53d12
truongdq <truongdq.dev@gmail.com>
2022-12-27 09:32:42 +0700

    feat: fix error path view

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from '../core/components/view';
+import { View } from '../core/components';
 
 export function HomeScreen() {
   return <View radii-2xl colors-amber-300 />;
